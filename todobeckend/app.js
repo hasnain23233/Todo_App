b7512c0052f5d2654b3ec4dc6314b3deed2dfffa
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-08-21 16:25:22 +0500

    handle the 404 page of the application

--- todobeckend/app.js
+++ todobeckend/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const ListRouter = require('./router/listRouter')
+const { errorhandle } = require('./controller/listController')
 
 
 app.use(ListRouter)
+app.use(errorhandle)
 
 app.listen(4200, () => {
     console.log('Server side running on the http://localhost:4200 check it')
--- todobeckend/controller/listController.js
+++ todobeckend/controller/listController.js
@@ -1,3 +1,7 @@
 exports.getListItems = (req, res, next) => {
     res.send('This request from controller of the website')
 }
+
+exports.errorhandle = (req, res, next) => {
+    res.status(404).send('Sorry, this page does not exist. Back to home.');
+};

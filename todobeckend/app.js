57ee47273c39610667d91bd27da07ad80816312a
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-08-21 16:29:11 +0500

    connecting to the database of the application

--- todobeckend/app.js
+++ todobeckend/app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 const ListRouter = require('./router/listRouter')
+const mongoose = require('mongoose')
 const { errorhandle } = require('./controller/listController')
+require('dotenv').config()
+
+const mongoLink = process.env.MONGO_URL
 
 
 app.use(ListRouter)
 app.use(errorhandle)
 
-app.listen(4200, () => {
-    console.log('Server side running on the http://localhost:4200 check it')
+mongoose.connect(mongoLink).then(() => {
+    console.log('connecting to the database of the website')
+    app.listen(4200, () => {
+        console.log('Server side running on the http://localhost:4200 check it')
+    })
+}).catch(() => {
+    console.log('sorry we couldn`t connect to the database of your application check it and try aging')
 })
